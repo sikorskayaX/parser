6d329fa9a0bebf25e51b4c2afd32cab2e8749e6e
sikorskayaX <sikorskaya02@gmail.com>
2023-11-30 02:58:55 +0300

    Refactor code and update data loading messages

--- app.js
+++ app.js
@@ -9,16 +9,33 @@ $(function(){
         });
     }
 
-    let updateSpbWeather = function(){
+    let updateEur = function(){
+        let request = {'url': 'https://www.cbr.ru/'};
+        $.get(api, request, function(response){
+            let result = $(response).find('._euro + div').text();
+            $('#eur > .value').text(result);
+        });
+    }
 
+    let updateSpbWeather = function(){
+        let request = {'url': 'https://pogoda.mail.ru/prognoz/sankt_peterburg/'};
+        $.get(api, request, function(response){
+            let result = $(response).find('.information__content__temperature').text();
+            $('#weather > .value').text(result + " C");
+        });
     }
 
     let updateWiki = function(){
-
+        let request = {'url': 'https://en.wikipedia.org/wiki/Russian_Wikipedia'};
+        $.get(api, request, function(response){
+            let result = $(response).find('[title="ru:Special:Statistics"]').first().text();
+            $('#wiki > .value').text(result);
+        });
     }
 
     let updateAllData = function(){
         updateUsd();
+        updateEur();
         updateSpbWeather();
         updateWiki();
     }
